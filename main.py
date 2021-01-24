ee4c841a5e9a98d62bc7d32b52f57a0c8c61d108
bocahkendil <51228319+bocahkendil@users.noreply.github.com>
2021-01-25 06:20:25 +0700

    fix error google name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 print ('Hello World')
 try:
-    r = requests.get('https://goo gle.com')
+    r = requests.get('https://google.com')
     print(r.status_code)
     if r.status_code==200:
         print(r.text)
